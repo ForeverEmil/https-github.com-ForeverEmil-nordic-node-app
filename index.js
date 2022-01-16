e8ab0010587d8e0cf173a387629869d7830719db
ForeverEmil <emil.nuretdinov@yandex.ru>
2022-01-16 14:03:36 +0300

    Добавили сервер

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 // run `node index.js` in the terminal
+// console.log(`Hello Node.js v${process.versions.node}!`);
 
-console.log(`Hello Node.js v${process.versions.node}!`);
-console.log('TEST');
-console.log('TEST2');
+// 1. ШАГ. Подключаем Express
+const express = require('express');
+
+// 2. ШАГ. Создаем объект приложения
+const app = express();
+
+// 4. ШАГ. Создаем первое route приложение
+// 4.1 ШАГ. Первый параметр get запроса  -
+// адрес страницы на которую мы переходим
+app.get('/', function (request, response) {
+  // console.log('req', request);
+  // console.log('res', response);
+  response.send('<h1>Hello Server</h1>');
+});
+// 3. ШАГ. Запуск приложения на орпеделенном порту
+app.listen(3000);
